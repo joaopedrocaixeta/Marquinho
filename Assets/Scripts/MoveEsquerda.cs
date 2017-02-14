c56f7cc6193dc53087712e277290092133516704
Joao Pedro <4ugusto.melo@gmail.com>
2017-02-14 12:31:25 -0200

    Implementando Pranchas

---
+++ Assets/Scripts/MoveEsquerda.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEsquerda : MonoBehaviour {
+
+    public float velocidade = 1f;
+    public float limite;
+    public float retorno;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 velocidadeVetorial = Vector3.left * velocidade;
+
+        transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
+
+        if (transform.position.x <= limite) {
+            transform.position = new Vector3(retorno, transform.position.y, transform.position.z);
+        }
+
+	}
+}
