f88df847872ddebaca955e15cea5018c87386ac4
Joao Pedro <j.p.a.caixeta@gmail.com>
2017-03-07 11:45:07 -0300

    Acrescentando sons

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float ForcaDoPulo = 10f;
+    public AudioClip somPulo;
+    public AudioClip somMorte;
     private Animator anim;
     private Rigidbody rb;
+    private AudioSource audioSource;
     private bool pulando = false;
 
 	void Start () {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 	
 	void Update () {
         if (Input.GetMouseButtonDown(0)) {
             anim.Play("pulando");
+            audioSource.PlayOneShot(somPulo);
             rb.useGravity = true;
             pulando = true;
         }
@@ -35,6 +40,7 @@ public class PlayerController : MonoBehaviour {
             rb.AddForce(new Vector3(-50f, 20f, 0f), ForceMode.Impulse);
             rb.detectCollisions = false;
             anim.Play("morrendo");
+            audioSource.PlayOneShot(somMorte);
         }
     }
 }
