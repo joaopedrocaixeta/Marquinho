49da075d1c390e65a137415bf5d2a86db94e1404
Joao Pedro <j.p.a.caixeta@gmail.com>
2017-03-07 12:08:51 -0300

    Implementando o GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+    public GameObject obstaculo;
+    public float espera;
+    public float tempoDestruicao;
+    public static GameController instancia = null;
+
+    void Awake() {
+        if (instancia == null) {
+            instancia = this;
+        }
+        else if (instancia != null) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+	void Start () {
+        StartCoroutine(GerarObstaculos());
+	}
+	
+	IEnumerator GerarObstaculos() {
+		while (true) {
+            Vector3 pos = new Vector3(8.5f, Random.Range(0f, 6f), 0f);
+            GameObject obj = Instantiate(obstaculo, pos, Quaternion.identity) as GameObject;
+            Destroy(obj, tempoDestruicao);
+            yield return new WaitForSeconds(espera);
+        }
+	}
+}
